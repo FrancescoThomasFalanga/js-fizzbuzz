d742ecc93530eedc4c0ba6b975458f2b49cb48c9
FrancescoThomasFalanga <francescoasdf3124@gmail.com>
2023-02-24 13:32:04 +0100

    init

---
+++ js/script.js
@@ -0,0 +1,11 @@
+/* Scrivi un programma che stampi in console i numeri da 1 a 100, ma che per i multipli di 3 stampi “Fizz” al posto del numero 
+e per i multipli di 5 stampi “Buzz”. Per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+*/
+
+/*
+- Inizializzazione programma
+- stampare in console i numeri da 1 a 100 e memorizzarli
+- per i multipli di 3 non bisognerà stampare il numero ma "Fizz" al posto di quest'ultimo
+- per i multipli di 5 non bisognerà stampare il numero ma "Buzz" al posto di quest'ultimo
+- per i multipli sia di 3 che di 5 non bisognerà stampare il numero ma "FizzBuzz" al posto di quest'ultimo
+*/
